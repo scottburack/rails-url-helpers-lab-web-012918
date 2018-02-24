4afbeb9997593abb90da8bd480389bca7ff90eb4
scott <you@example.comitsmescottb123@gmail.com>
2018-02-24 15:56:36 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,23 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    set_student
+    if @student.active == false
+      @student.update(active: true)
+    else
+      @student.update(active: false)
+    end
+
+    redirect_to "/students/#{@student.id}"
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+scott = Student.create(first_name: 'Scott', last_name: 'Burack')
